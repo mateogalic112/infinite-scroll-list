b93f903605da72fe412a11f0632e86cad7ae610c
matteoo.eth <mateogalic112@gmail.com>
2024-07-07 11:38:22 +0200

    Fix hidration error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // Ensure that the component only renders on the client
+  if (!isMounted) return null;
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
